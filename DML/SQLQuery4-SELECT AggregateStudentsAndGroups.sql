2c9b3f08c0e61fdb0c9e4fd5c3d842f20b0633f1
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-02-18 21:37:00 +0300

    DML-Groups and Students

---
+++ DML/SQLQuery4-SELECT AggregateStudentsAndGroups.sql
@@ -0,0 +1,28 @@
+﻿--SQLQuery4-SELECT AggregateStudentsAndGroups.sql
+USE PV_521_Import;
+
+SELECT
+    direction_name    AS 'Направление обучения'
+    ,COUNT(DISTINCT group_id) AS 'Количество групп'
+    ,COUNT(stud_id)    AS 'Количество студентов'
+
+FROM Students, Groups, Directions
+
+WHERE [group] = group_id
+
+AND direction = direction_id
+
+GROUP BY direction_name
+
+SELECT
+    direction_name    AS  N'Направление обучения'
+    --,COUNT(stud_id)    AS  N'Количество студентов'
+    --,COUNT(DISTINCT group_id) AS  N'Количество групп'
+    ,(SELECT COUNT(DISTINCT group_id) FROM Groups WHERE direction=direction_id) AS N'Количество групп'
+    ,(
+    SELECT COUNT(DISTINCT stud_id)
+    FROM Students,Groups--,Directions
+    WHERE [group]=group_id
+    AND direction=direction_id
+    )   AS N'Количество студентов'
+FROM Directions
