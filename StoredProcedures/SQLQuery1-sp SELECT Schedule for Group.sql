e3ae0ca6831ce4b4e481c862cb0bb129cb8331f7
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-03-13 18:44:20 +0300

    1

--- StoredProcedures/SQLQuery1-sp SELECT Schedule for Group.sql
+++ StoredProcedures/SQLQuery1-sp SELECT Schedule for Group.sql
@@ -4,23 +4,22 @@ USE PV_521_Import;
 SET DATEFIRST 1;
 GO
 
-CREATE OR ALTER PROCEDURE sp_SelectScheduleFor @group	AS	NCHAR(10)
+ALTER PROCEDURE sp_SelectScheduleFor @group	AS	NCHAR(10)
 AS
 BEGIN
-	DECLARE @group_id	AS INT	=	(SELECT group_id FROM Groups WHERE group_name LIKE @group);
+	DECLARE	@group_id	AS	INT	=	(SELECT group_id FROM Groups WHERE	group_name	LIKE @group);
 	SELECT
-		[Группа]	=	group_name,
-		[Дисциплина]	=	discipline_name,
-		[Дата]	=	[date],
-		[Время]	=	[time],
-		[День]	=	DATENAME(WEEKDAY,[date]),
-		[Препод]	=	FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name),
-		[Статус]	=	IIF(spent=1, N'Проведено',N'Запланировано')
-
-	FROM Schedule,Groups,Teachers,Disciplines
-	WHERE [group] = group_id
-	AND [group]	= @group_id
-	AND discipline = discipline_id
-	AND teacher = teacher_id
-	
+			[Группа]	=	group_name,
+			[Дисциплина]=	discipline_name,
+			[Дата]		=	[date],
+			[Время]		=	[time],
+			[День]		=	DATENAME(WEEKDAY,[date]),
+			[Препод]	=	FORMATMESSAGE(N'%s %s %s',last_name,first_name,middle_name),
+			[Статус]	=	IIF(spent=1, N'Проведено', N'Запланировано')
+	FROM	Schedule,Groups,Teachers,Disciplines
+	WHERE	[group]		=	group_id
+	AND		[group]		=	@group_id
+	AND		discipline	=	discipline_id
+	AND		teacher		=	teacher_id
+	ORDER BY [date]
 END
